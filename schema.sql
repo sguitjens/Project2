72b802d7aa739f99eea653d1e2130b9114867317
Jennifer Kim <jenniferkim.businessonly@gmail.com>
2020-02-10 22:59:39 -0800

    sql

--- schema.sql
+++ schema.sql
@@ -1,3 +1,13 @@
 DROP DATABASE IF EXISTS nodelogin;
 CREATE DATABASE nodelogin;
 USE nodelogin;
+
+CREATE TABLE `accounts` (
+  `id` Int (11) AUTO INCREMENT NOT NULL,
+  `username` VARCHAR (50) NOT NULL,
+  `password` VARCHAR (255) NOT NULL,
+  `email` VARCHAR (100) NOT NULL,
+  PRIMARY KEY ('id')
+) 
+
+INSERT INTO `accounts` (`id`, `username`, `password`, `email`) VALUES (1, 'test', 'test', 'test@test.com');
