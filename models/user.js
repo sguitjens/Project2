f0be9685effd86783c1b5f6298f25dbeffe2a565
Sydney Guitjens <sguitjens@gmail.com>
2020-02-10 19:43:48 -0800

    started adding sequelize code

---
+++ models/user.js
@@ -0,0 +1,45 @@
+/*eslint prefer-const: "error"*/
+/*eslint-env es6*/
+const bcrypt = require("bcryptjs");
+
+module.exports = function(sequelize, DataTypes) {
+  const User = sequelize.define("User", {
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    firstName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    username: {
+      type: DataTypes.STRING,
+      allwoNull: false
+    }
+  });
+
+  User.prototype.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+  };
+
+  User.addHook("beforeCreate", function(user) {
+    user.password = bcrypt.hashSync(
+      user.password,
+      bcrypt.genSaltSync(10),
+      null
+    );
+  });
+  return User;
+};
